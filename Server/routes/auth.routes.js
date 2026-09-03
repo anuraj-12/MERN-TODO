b4a2ac70af62522ae3fb27e9de59fbdd82da8cfb
Anuraj <chhetrianuraj410@gmail.com>
2026-09-03 16:54:38 +0545

    fix error

--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -1,8 +1,6 @@
 
 import Router from "express"
 import * as authController from "../controller/auth.controller.js"
-import {login , singUp} from "../validator/zod.validate.js"
-import {validate} from "../middleware/validate-middleware.js"
 import {authMiddileware} from "../middleware/auth-middleware.js"
 
 
@@ -10,8 +8,8 @@ import {authMiddileware} from "../middleware/auth-middleware.js"
 const router  = Router();
 
 
-router.route("/register").post(validate(singUp) ,authController.signUpUser)
-router.route("/login").post(validate(login) ,authController.loginUser)
+router.route("/register").post(authController.signUpUser)
+router.route("/login").post(authController.loginUser)
 router.route("/logout").post(authController.userLogout)
 router.route("/refresh").post(authController.refreshToken)
 
